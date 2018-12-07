9b36de7ec9f7cb31cc2e52a49f70542d1067475a
kamdjouduplex <tony14pro@gmail.com>
2018-12-07 16:09:41 +0100

    edited assignments21

--- assigment23.js
+++ assigment23.js
@@ -8,28 +8,34 @@
 // 6. if gest we pop congratulation
 // 7. we add the loop to do it atmost 7 times
 
-let generatedNumber = Math.floor( Math.random() * 100);
-
-function guestedNumber(g_num) {
-	let u_num = Number(prompt("guest the number"));
-	if (u_num == g_num) {
-		alert("congratulation");
-		return;
-	} else if(u_num > g_num){
-		alert("your gest number is bigger, try again");
-		guestedNumber();
-	} else if(u_num < g_num){
-		alert("your gest number is smaller, try again");
-		guestedNumber();
+let g_num = Math.floor( Math.random() * 100);
+	let count = 1;
+	function guestedNumber() {
+		
+		while(count <= 7){
+			let u_num = Number(prompt("guest the number ("+count+") chances"));
+			
+			if (u_num == g_num) {
+				alert("Congratulation you win, Play again ?");
+			} else if(u_num > g_num){
+				alert("your gest number is bigger, try again");
+				count++
+				if (count != 7) {
+					guestedNumber();
+				}else{
+					alert("You lost the game. Play again ?");
+				}
+				
+			} else if(u_num < g_num){
+				alert("your gest number is smaller, try again");
+				count++
+				if (count != 7) {
+					guestedNumber();
+				}else{
+					alert("You lost the game. Play again ?");
+				}
+			}
+		}
+		
 	}
-}
-
-let i = 1;
-do{
-	guestedNumber(generatedNumber);
-}while(i <= 7){
-	guestedNumber(generatedNumber);
-}
-
-
-
+	guestedNumber();
